65b6637d617ae94d2917c560053fdd4fef1866b0
kainar <masujimk@gmail.com>
2020-10-29 17:26:42 +0600

    submit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const connect = () => {
     Axios({
       method: "get",
-      url: "http://localhost:8000/api/kaznu/start-exam/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDU3IiwibmFtZSI6Imthem51IiwiZHVyYXRpb24iOjYwLCJ0aW1lb3BlbiI6MTYwMzY3MDQwMCwidGltZWNsb3NlIjoxNjAzOTI5NjAwLCJleGFtX25hbWUiOiJCaW9sb2d5IiwicnVsZXMiOnsiZmFjZV9yZWMiOmZhbHNlLCJzY3JlZW4iOnRydWUsImxpdmVfY2hhdCI6ZmFsc2UsImF1ZGlvIjp0cnVlLCJzdHJlYW0iOnRydWUsImF1dGhvcml6ZSI6ZmFsc2UsImNsaXBib2FyZCI6dHJ1ZSwiZHVhbF9zY3JlZW4iOmZhbHNlLCJtb2JpbGUiOmZhbHNlfSwidXJsIjoiaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8iLCJzdWJtaXRfdXJsIjoiaHR0cHM6Ly90aGUtc3RlcHBlLmNvbS8iLCJjaGVhdGluZ19jb2RlIjoxMDA4fQ.vODJZHEkBg7TMWRxN8txqEGCJxNu6I5wrEV-Wd437jg",
+      url: "http://localhost:8000/api/kaznu/start-exam/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiIxMDU3IiwibmFtZSI6Imthem51IiwiZHVyYXRpb24iOjYwLCJ0aW1lb3BlbiI6MTYwMzY3MDQwMCwidGltZWNsb3NlIjoxNjA0MDI5NjAwLCJleGFtX25hbWUiOiJCaW9sb2d5IiwicnVsZXMiOnsiZmFjZV9yZWMiOmZhbHNlLCJzY3JlZW4iOnRydWUsImxpdmVfY2hhdCI6ZmFsc2UsImF1ZGlvIjp0cnVlLCJzdHJlYW0iOnRydWUsImF1dGhvcml6ZSI6ZmFsc2UsImNsaXBib2FyZCI6dHJ1ZSwiZHVhbF9zY3JlZW4iOmZhbHNlLCJtb2JpbGUiOmZhbHNlfSwidXJsIjoiaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8iLCJzdWJtaXRfdXJsIjoiaHR0cHM6Ly90aGUtc3RlcHBlLmNvbS8iLCJjaGVhdGluZ19jb2RlIjoxMDA4LCJqdGkiOiJmMWNlYWMxYi0yNDI4LTQ5Y2UtYWNjMS0zOTI5NWE4YjFkOTMiLCJpYXQiOjE2MDM5NzAzNjYsImV4cCI6MTYwMzk3Mzk2Nn0.9aNh9SceW1x-iPp3SntOCvIOcFTCQV6qpDIcl5RdZwc",
       withCredentials: true,
     })
       .then((res) => {
@@ -40,6 +40,8 @@ function App() {
         })}
       </ul> */}
       <button onClick={connect}>Start</button>
+      <button onClick={()=>window.location.replace(`http://localhost:3000/submit-quiz/?cheating_code=${'1008'}&redirectUrl=http://localhost:3001`)}>submit</button>
+
     </>
   );
 }
